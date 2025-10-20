d1e27886abf213f945f2b5c20c08695e1c1244de
LipeMelo84 <88955286+LipeMelo84@users.noreply.github.com>
2025-10-20 20:01:22 -0300

    Delete src directory

--- src/main/java/com/felipe/reiz/tarefa/repository/ProntuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.felipe.reiz.tarefa.repository;
-
-import com.felipe.reiz.tarefa.model.Prontuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProntuarioRepository extends JpaRepository<Prontuario, Long> {
-}
--- src/main/java/com/felipe/reiz/tarefa/repository/TarefaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.felipe.reiz.tarefa.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.felipe.reiz.tarefa.model.Tarefa;
-
-public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
-}
